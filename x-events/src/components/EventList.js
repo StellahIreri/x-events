dfe03787795bd6b77943bc6176a8a2f1e02c36eb
StellahIreri <stellahmireri@gmail.com>
2023-07-28 15:17:48 +0300

    axios.get

--- x-events/src/components/EventList.js
+++ x-events/src/components/EventList.js
@@ -7,7 +7,7 @@ const EventList = () => {
 
   useEffect(() => {
     // Fetch events from the backend API
-    axios.get('http://localhost:9292/events')
+    axios.get('http://localhost:9292/event')
       .then((response) => {
         setEvents(response.data);
       })
